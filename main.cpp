5c6bccdd91025d1f5f27ba23aa192e3c15ce046d
orlandovaldez <orlando.valdez@my.tccd.edu>
2020-02-20 01:54:42 +0000

    results and prototype call

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
+//Orlando Valdez 2-19-2020
+//COSC 1436 
+//Unit 6 Assignment Sorting
 #include <iostream>
+#include "accounts.h"
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+void printArray(int[], int);
+void sortArrayAsc(int[], int);
+
+
+int main() 
+{
+  //Bubble sort ASC method per Task 2 of Unit 6 Assignment
+  cout << "\nStart: " << cpuTime() << endl;
+  
+  sortArrayAsc(accountBalances, maxAccounts);//Call the sort Function
+  printArray(accountBalances, maxAccounts);
+  cout << "\nEnd: " << cpuTime() << endl;
+
+
+  return 0; 
 }
+
+void printArray(int arrayVals[], int size)
+{
+  cout << "\nPrinted Values in Array: " << endl;
+  for(int i = 0; i < size; i++)
+  {
+    cout << arrayVals[i] << ",";
+
+  }
+  
+}
+
+void sortArrayAsc(int arrayVals [], int size)
+{
+  int temp = 0; 
+  //Bubble Sort Asc a la Dr. Rajan Alex
+  for(int left = 0; left < size; left++) 
+    
+    for(int right = left+1; right < size; right++)
+    {
+
+      
+    if(arrayVals[left] < arrayVals[right])
+      {
+        temp = arrayVals[left];
+        arrayVals[left] = arrayVals[right];
+        arrayVals[right] = temp;
+
+      }
+    }
+  }
+  
+  
